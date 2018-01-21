e88eabc6e6a2538c26237c2be72b1b24294891f7
Alfred Melch <alfred.melch@gmx.de>
2018-01-21 13:38:08 +0100

    restructure setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
 from setuptools import setup, find_packages
 
-
-def readme():
-    with open('README.md') as f:
-        return f.read
-
-
 setup(
     name='flask-expects-json',
-    description='Decorator for REST endpoints in flask. Validate request data and store in g',
-    long_description=readme(),
+    version='0.1.0',
+    description='Decorator for REST endpoints in flask. Validate JSON request data.',
     url='https://github.com/fischerfredl/flask-expects-json',
-    download_url='https://github.com/fischerfredl/flask-expects-json/archive/0.1.tar.gz',
-
-    version='0.1',
-    license='MIT',
-
     author='Alfred Melch',
     author_email='alfred.melch@gmx.de',
-
+    license='MIT',
+    classifiers=[],
+    keywords='',
     packages=find_packages(exclude=['tests.*', 'tests']),
-
-    # dependencies
     install_requires=['flask', 'jsonschema'],
-    dependency_links=[],
 
     test_suite='tests.test_suite',
     tests_require=[],
