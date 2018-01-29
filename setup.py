2f738c723552cccde6c864eb8b04360dd17d4f40
Alfred Melch <alfred.melch@gmx.de>
2018-01-29 16:40:59 +0100

    push version add ci stages

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='flask-expects-json',
-    version='1.0.5',
+    version='1.0.6',
     description='Decorator for REST endpoints in flask. Validate JSON request data.',
     long_description=readme(),
     url='https://github.com/fischerfredl/flask-expects-json',
@@ -19,16 +19,15 @@ setup(
         'Framework :: Flask',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
-    keywords=['flask', 'json', 'validation', 'schema'],
+    keywords=['flask', 'json', 'validation', 'schema', 'jsonschema'],
     packages=find_packages(exclude=['tests.*', 'tests']),
     install_requires=[
         'flask>=0.12.2',
         'jsonschema>=2.6.0'
     ],
-
-    test_suite='tests.test_suite',
-    tests_require=[],
+    test_suite='tests.test_suite'
 )
