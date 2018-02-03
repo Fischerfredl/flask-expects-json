0786d6902a519447770a3815ad2a87b7cf557a4a
Alfred Melch <alfred.melch@gmx.de>
2018-02-03 13:22:58 +0100

    refactor tests

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -4,25 +4,27 @@ import flask
 
 from flask_expects_json import expects_json
 
-schema = {
+
+class TestExpects(unittest.TestCase):
+    def setUp(self):
+        self.app = flask.Flask(__name__)
+
+        @self.app.route('/')
+        @expects_json()
+        def no_schema():
+            return 'happy'
+
+        @self.app.route('/schema')
+        @expects_json({
             "type": "object",
             "properties": {
                 "price": {"type": "number"},
                 "name": {"type": "string"},
             },
             "required": ["price"]
-        }
-
-
-class TestExpects(unittest.TestCase):
-    def setup_route(self, schema):
-        @self.app.route('/')
-        @expects_json(schema)
+        })
         def index():
             return 'happy'
-
-    def setUp(self):
-        self.app = flask.Flask(__name__)
 
         self.client = self.app.test_client()
         self.ctx = self.app.app_context()
@@ -32,38 +34,25 @@ class TestExpects(unittest.TestCase):
         self.ctx.pop()
 
     def test_valid_decorator(self):
-        self.setup_route(None)
         response = self.client.get('/')
         self.assertEqual(response.status_code, 400)
         response = self.client.get('/', data=json.dumps(dict()))
         self.assertEqual(response.status_code, 200)
 
     def test_validation_valid(self):
-        self.setup_route(schema)
-        response = self.client.get('/', data=json.dumps({"name": "Eggs", "price": 34.99}))
+        response = self.client.get('/schema', data=json.dumps({"name": "Eggs", "price": 34.99}))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(flask.g.data['name'], 'Eggs')
         self.assertEqual(flask.g.data['price'], 34.99)
 
     def test_validation_invalid(self):
-        self.setup_route(schema)
-        response = self.client.get('/', data=json.dumps({"name": "Eggs", "price": 'invalid'}))
+        response = self.client.get('/schema', data=json.dumps({"name": "Eggs", "price": 'invalid'}))
         self.assertEqual(response.status_code, 400)
 
     def test_missing_parameter(self):
-        self.setup_route(schema)
-        response = self.client.get('/', data=json.dumps({"name": "Eggs"}))
+        response = self.client.get('/schema', data=json.dumps({"name": "Eggs"}))
         self.assertEqual(response.status_code, 400)
 
     def test_additional_parameter(self):
-        self.setup_route(schema)
-        response = self.client.get('/', data=json.dumps({"name": "Eggs", "price": 34.99}))
-        self.assertEqual(response.status_code, 200)
-
-    def test_no_schema(self):
-        @self.app.route('/')
-        @expects_json()
-        def index():
-            return 'happy'
-        response = self.client.get('/', data=json.dumps(dict()))
+        response = self.client.get('/schema', data=json.dumps({"name": "Eggs", "price": 34.99}))
         self.assertEqual(response.status_code, 200)
