038fefed3b93ba7ddf5f350930bc11925def3f14
Alfred Melch <code@melch.pro>
2019-05-22 09:38:17 +0200

    Remove python3.4 support

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         'Framework :: Flask',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
