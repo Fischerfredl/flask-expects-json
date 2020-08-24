57610c50781c133b820d813e421aed0df31429d4
Michael Theos <theos.michael@gmail.com>
2020-08-24 20:50:48 +1000

    Added test for ignore methods

--- tests/test_ignore.py
+++ tests/test_ignore.py
@@ -8,12 +8,12 @@ class TestExpects(unittest.TestCase):
     def setUp(self):
         self.app = flask.Flask(__name__)
 
-        @self.app.route('/')
+        @self.app.route('/', methods=['GET', 'HEAD', 'POST'])
         @expects_json()
         def all_requests():
             return 'happy'
 
-        @self.app.route('/ignore_one', methods=['GET', 'POST'])
+        @self.app.route('/ignore_one', methods=['GET', 'HEAD', 'POST'])
         @expects_json(ignore_for=['GET'])
         def ignore_get():
             return 'happy'
@@ -31,15 +31,21 @@ class TestExpects(unittest.TestCase):
         response = self.client.get('/')
         self.assertEqual(400, response.status_code)
         self.assertIn('Failed to decode', response.data.decode())
+        response = self.client.head('/')
+        self.assertEqual(400, response.status_code)
         response = self.client.get('/', data='{}', content_type='application/json')
         self.assertEqual(200, response.status_code)
 
     def test_ignore_one(self):
         response = self.client.get('/ignore_one')
         self.assertEqual(200, response.status_code)
-        response = self.client.post('/ignore_one', data='')
-        self.assertIn('Failed to decode', response.data.decode())
+        response = self.client.head('/ignore_one')
         self.assertEqual(400, response.status_code)
+        response = self.client.post('/ignore_one', data='')
+        self.assertEqual(400, response.status_code)
+        self.assertIn('Failed to decode', response.data.decode())
+        response = self.client.get('/ignore_one', data='{}', content_type='application/json')
+        self.assertEqual(200, response.status_code)
 
     def test_ignore_multiple(self):
         response = self.client.get('/ignore_two')
@@ -47,5 +53,7 @@ class TestExpects(unittest.TestCase):
         response = self.client.head('/ignore_two')
         self.assertEqual(200, response.status_code)
         response = self.client.post('/ignore_two', data='')
-        self.assertIn('Failed to decode', response.data.decode())
         self.assertEqual(400, response.status_code)
+        self.assertIn('Failed to decode', response.data.decode())
+        response = self.client.get('/ignore_two', data='{}', content_type='application/json')
+        self.assertEqual(200, response.status_code)
